2415654b80b0ec4f14b0466edf39e3c2a92cdb50
kishinokazuo <kishinokazuo@gmail.com>
2018-01-04 22:51:15 +0900

    詳細ページと編集機能を追加。しかし編集機能が正しく動かない（何を入れてもメーカ名が消えるだけ・・・）

--- app/controllers/specs_controller.rb
+++ app/controllers/specs_controller.rb
@@ -16,6 +16,21 @@ class SpecsController < ApplicationController
     spec.destroy
   end
 
+  def show
+    @spec = Spec.find(params[:id])
+    @spec_attribute = Spec.attribute_names
+  end
+
+  def edit
+    @edit_id = params[:id]
+    @edit_spec = Spec.find(params[:id])
+  end
+
+  def update
+    spec = Spec.find(params[:id])
+    spec.update(spec_params)
+  end
+
   private
   def spec_params
     params.permit(:id, :Make, :Model, :Model_version, :Years_sold, :Cylinders, :Displacement, :Engine_type, :Power_kW, :Power_rpm, :Torque_Nm, :Torque_rpm)
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get 'specs/new' => 'specs#new'
   post 'specs' => 'specs#create'
   delete 'specs/:id' => 'specs#destroy'
+  get 'specs/:id/edit' => 'specs#edit'
+  patch 'specs/:id/edit' => 'specs#update'
+  get 'specs/show/:id/' => 'specs#show'
 end
